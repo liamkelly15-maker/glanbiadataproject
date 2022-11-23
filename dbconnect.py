b9529c43724b7c66e332ba65137315e2bdaae642
Kelly <liamkelly@glanbia.net>
2022-11-23 15:04:12 +0000

    latest

--- dbconnect.py
+++ dbconnect.py
@@ -1,6 +1,10 @@
 import pyodbc
 import pandas as pd
 import numpy as np
+from fpdf import FPDF
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 
 #define a dict
@@ -77,6 +81,26 @@ df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
 df.loc['Gooding']=df1.loc[0].values
 
 
-print(df)
+
+
+alternating_colors = [['white'] * len(df.columns), ['lightgray'] * len(df.columns)] * len(df)
+alternating_colors = alternating_colors[:len(df)]
+
+#https://stackoverflow.com/questions/32137396/how-do-i-plot-only-a-table-in-matplotlib
+fig, ax =plt.subplots(figsize=(12,4))
+ax.axis('tight')
+ax.axis('off')
+the_table = ax.table(cellText=df.values,rowLabels=df.index,colLabels=df.columns,rowColours=['lightblue']*len(df),
+                        colColours=['lightblue']*len(df.columns),
+                        cellColours=alternating_colors,loc='center')
+
+
+#https://stackoverflow.com/questions/4042192/reduce-left-and-right-margins-in-matplotlib-plot
+pp = PdfPages("report.pdf")
+pp.savefig(fig, bbox_inches='tight')
+pp.close()
+
+#print(df.values)
+#print (df.columns)
 #print(type(df1))
 
