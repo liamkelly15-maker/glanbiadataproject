41f0fd8a52bfd30f7d01b1798fb6d6d8a98d9bef
Kelly <liamkelly@glanbia.net>
2022-11-23 14:24:23 +0000

    latest

--- dbconnect.py
+++ dbconnect.py
@@ -2,22 +2,81 @@ import pyodbc
 import pandas as pd
 import numpy as np
 
-conn1 = pyodbc.connect('Driver={SQL Server};'
+
+#define a dict
+dict1=dict({'Site':['Ballyragget', 'Belview','Drogheda','Ballitore','Wexford','loughegish','Virginia','Gooding'], 'No of Containers':[0,0,0,0,0,0,0,0]})
+#create a dataframe from dict
+df=pd.DataFrame.from_dict(dict1).set_index('Site')
+
+
+conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=brgmes02;'
                       'Database=iqs;'
                       'Trusted_Connection=yes;')
 
-df1 = pd.read_sql_query('SELECT * FROM iqs.container', conn1)
 
-conn2 = pyodbc.connect('Driver={SQL Server};'
+df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
+df.loc['Ballyragget']=df1.loc[0].values
+
+
+conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=blvmes02;'
                       'Database=iqs;'
                       'Trusted_Connection=yes;')
 
-df2 = pd.read_sql_query('SELECT * FROM iqs.container', conn2)
+df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
+df.loc['Belview']=df1.loc[0].values
 
-df1=df1.append(df2)
 
-print(df1)
-print(type(df1))
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=drgmes02;'
+                      'Database=iqs;'
+                      'Trusted_Connection=yes;')
+
+df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
+df.loc['Drogheda']=df1.loc[0].values
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=bltmes02;'
+                      'Database=iqs;'
+                      'Trusted_Connection=yes;')
+
+df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
+df.loc['Ballitore']=df1.loc[0].values
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=wxcmes02;'
+                      'Database=iqs;'
+                      'Trusted_Connection=yes;')
+
+df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
+df.loc['Wexford']=df1.loc[0].values
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=mgnmes02;'
+                      'Database=iqs;'
+                      'Trusted_Connection=yes;')
+
+df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
+df.loc['loughegish']=df1.loc[0].values
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=virmes02;'
+                      'Database=iqs;'
+                      'Trusted_Connection=yes;')
+
+df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
+df.loc['Virginia']=df1.loc[0].values
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=gdpmes12;'
+                      'Database=iqs;'
+                      'Trusted_Connection=yes;')
+
+df1=pd.read_sql_query('SELECT count(*) FROM iqs.container', conn)
+df.loc['Gooding']=df1.loc[0].values
+
+
+print(df)
+#print(type(df1))
 
